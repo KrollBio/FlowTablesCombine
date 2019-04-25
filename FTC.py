7445a4c9e124e5fa8b460363173cfbdac718454b
KrollBIO <kylekroll@outlook.com>
2019-04-25 11:06:41 -0400

    Added checks for input directory existing and separator being valid.

--- FTC.py
+++ FTC.py
@@ -10,14 +10,15 @@
 from os import listdir
 from os.path import isfile
 from os.path import join
+from os.path import isdir
 import pandas as pd
 import argparse
-
-# Load CSV and remove the Mean and SD rows
-# Split column names to remove the unneccessarily long gating paths
+import sys
 
 
 def load_csv(filepath):
+    # Load CSV and remove the Mean and SD rows
+    # Split column names to remove the unnecessarily long gating paths
     temp_df = pd.read_csv("{0}{1}".format(file_dir, filepath), delimiter=",")
     temp_df = temp_df.drop(temp_df[temp_df.iloc[:,0] == "Mean"].index)
     temp_df = temp_df.drop(temp_df[temp_df.iloc[:, 0] == "SD"].index)
@@ -27,10 +28,9 @@ def load_csv(filepath):
     temp_df.columns = new_colnames
     return(temp_df)
 
-# Split column name and take last item in split
-
 
 def modify_col_names(colnames):
+    # Split column name and take last item in split
     split_name = colnames.split("/")
     return(split_name[len(split_name)-1])
 
@@ -41,22 +41,25 @@ def main():
     concat_df = pd.concat(list_dfs, sort=False, ignore_index=True)
     concat_df.to_csv(output_file, sep=file_sep, index=False)
 
-# Adding args to command line to specify input file directory, output file, and separator to use
-
 
 if __name__ == '__main__':
+    # Adding args to command line to specify input file directory, output file, and separator to use
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", help="Input Directory", type=str, required=True)
     parser.add_argument("--output", "-o", help="Output filename", type=str, required=True)
     parser.add_argument("--separator", "-s", help="Separator, options: [csv] [tab]", required=False, default=",", type=str)
     args = parser.parse_args()
     file_dir = args.input + "/"
+    if not isdir(file_dir):
+        print("Input directory does not exist\nRun 'python FTC.py -h' for help.")
+        sys.exit(0)
     if args.separator == "csv":
         file_sep = ","
     elif args.separator == "tab":
         file_sep = "\t"
     else:
-        print("Invalid file output separator supplied.")
+        print("Invalid file output separator supplied.\nRun 'python FTC.py -h' for help.")
+        sys.exit(0)
     output_file = args.output
 
     main()
